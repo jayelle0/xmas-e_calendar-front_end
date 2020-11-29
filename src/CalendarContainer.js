b792fb14acea51ce2146c64e32f06aa758ec6f47
jrwatson94 <j.watsonreid@gmail.com>
2020-11-29 17:47:08 -0500

    container component feature

--- src/CalendarContainer.js
+++ src/CalendarContainer.js
@@ -2,8 +2,18 @@ import React from 'react'
 import Day from './Day.js'
 
 class CalendarContainer extends React.Component{
+    renderCalendar = () => {
+        
+        const currentCalendar = this.props.calendars.filter(calendar => calendar.id == this.props.match.params.id)
+        console.log(currentCalendar)
+        return currentCalendar.days.map(day => <Day {...day}/>)
+    }
     render() {
-        return "Calendar Container"
+        return (
+            <div className="container">
+                {this.renderCalendar()}
+            </div>
+        )
     }
 }
 
--- src/Day.js
+++ src/Day.js
@@ -2,8 +2,13 @@ import React from 'react'
 import DayForm from './Dayform.js'
 
 class Day extends React.Component {
+    
     render() {
-        return "Day"
+        return (
+            <div className="day">
+                <h5>{this.props.date}</h5>
+            </div>
+        )
     }
 }
 
